9026435647f540b3968023bff3275b55870a874b
Aleksandr Gurinov <a.gurinov@corp.mail.ru>
2020-10-23 18:16:50 +0300

    fff

--- scripts/dotfiles-bootstrap.sh
+++ scripts/dotfiles-bootstrap.sh
@@ -13,77 +13,78 @@ GIT_WORK_TREE="${HOME}/dotfiles"
 # dotfiles_reset just ensures that dotfiles dir
 # will be present and empty.
 function dotfiles_reset() {
-        mkdir -p $GIT_WORK_TREE
-        rm -rf "${GIT_WORK_TREE}/"
+	mkdir -p $GIT_WORK_TREE
+	rm -rf "${GIT_WORK_TREE}/"
 }
 
 # dotfiles_install_git installs source code via git.
 function dotfiles_install_git() {
-        if [ -d "${HOME}/dotfiles/.git" ]; then
-                # git repo initialized, reset all changes.
-                git \
-                        --git-dir=$GIT_DIR \
-                        --work-tree=$GIT_WORK_TREE \
-                        clean -fxd
+	if [ -d "${HOME}/dotfiles/.git" ]; then
+		# git repo initialized, reset all changes.
+		git \
+			--git-dir=$GIT_DIR \
+			--work-tree=$GIT_WORK_TREE \
+			clean -fxd
 
-                git \
-                        --git-dir=$GIT_DIR \
-                        --work-tree=$GIT_WORK_TREE \
-                        pull \
-                        origin master
-        else
-                dotfiles_reset
+		git \
+			--git-dir=$GIT_DIR \
+			--work-tree=$GIT_WORK_TREE \
+			pull \
+			origin master
+	else
+		dotfiles_reset
 
-                git clone \
-                        $GIT_URL \
-                        $GIT_WORK_TREE
-        fi
+		git clone \
+			$GIT_URL \
+			$GIT_WORK_TREE
+	fi
 }
 
 # dotfiles_install_curl uses curl to download sources as tar.
 function dotfiles_install_curl() {
-        dotfiles_reset
+	dotfiles_reset
 
-        curl $TAR_URL \
-                --location \
-                --create-dirs \
-                --output ${GIT_WORK_TREE}/dotfiles.tar.gz \
-                --fail # return nonzero exit code if 404 or any error from server.
+	curl $TAR_URL \
+		--location \
+		--create-dirs \
+		--output ${GIT_WORK_TREE}/dotfiles.tar.gz \
+		--fail # return nonzero exit code if 404 or any error from server.
 
-        tar -xv \
-                --strip 1 \
-                -f ${GIT_WORK_TREE}/dotfiles.tar.gz \
-                -C ${GIT_WORK_TREE}
+	tar -xv \
+		--strip 1 \
+		-f ${GIT_WORK_TREE}/dotfiles.tar.gz \
+		-C ${GIT_WORK_TREE}
 
-        rm -rf ${GIT_WORK_TREE}/dotfiles.tar.gz
+	rm -rf ${GIT_WORK_TREE}/dotfiles.tar.gz
 }
 
 # dotfiles_install_curl wget curl to download sources as tar.
 function dotfiles_install_wget() {
-        dotfiles_reset
+	dotfiles_reset
 
-        wget $TAR_URL \
-                -O ${GIT_WORK_TREE}/dotfiles.tar.gz
+	wget $TAR_URL \
+		-O ${GIT_WORK_TREE}/dotfiles.tar.gz
 
-        tar -xv \
-                --strip 1 \
-                -f ${GIT_WORK_TREE}/dotfiles.tar.gz \
-                -C ${GIT_WORK_TREE}
+	tar -xv \
+		--strip 1 \
+		-f ${GIT_WORK_TREE}/dotfiles.tar.gz \
+		-C ${GIT_WORK_TREE}
 
-        rm -rf ${GIT_WORK_TREE}/dotfiles.tar.gz
+	rm -rf ${GIT_WORK_TREE}/dotfiles.tar.gz
 }
 
 # Choose proper way to get source files.
 if [ -n `command -v git` ]; then
-        dotfiles_install_git
+	dotfiles_install_git
 elif [ -n `command -v curl` ]; then
-        dotfiles_install_curl
+	dotfiles_install_curl
 elif [ -n `command -v wget` ]; then
-        dotfiles_install_wget
+	dotfiles_install_wget
 else
-        echo 'Cannot get dotfiles sources'
-        exit 1
+	echo 'Cannot get dotfiles sources'
+	exit 1
 fi
 
 # Install them on system.
 make -C "${HOME}/dotfiles"
+
